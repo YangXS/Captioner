7c5fd55af4590c4ef4027834ec4b3d2c704bfd36
gftm <goncalomordido@gmail.com>
2017-07-23 11:51:38 +0100

    Updated UI

--- captioner/app/src/main/java/com/ImageCaptioning/MainActivity.java
+++ captioner/app/src/main/java/com/ImageCaptioning/MainActivity.java
@@ -9,6 +9,9 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -22,15 +25,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ImageCaptioning.captioner.R;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends Activity {
     private static final String LOG_TAG = "MainActivity";
@@ -38,10 +47,19 @@ public class MainActivity extends Activity {
     private static final int REQUEST_IMAGE_SELECT = 200;
     public static final int MEDIA_TYPE_IMAGE = 1;
 
+    //camera variables start here
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private Camera.PictureCallback mPicture;
+    private FrameLayout preview;
+    private File pictureFile;
+    private Button captureButton;
+    //Camera variables end here
     private Button btnCamera;
     private Button btnSelect;
+    private Button btnRepeat;
     private Button btnSpeak;
-    private ImageView ivCaptured;
+    //private ImageView ivCaptured;
     private TextView tvLabel;
     private Uri fileUri;
     private ProgressDialog dialog;
@@ -50,7 +68,9 @@ public class MainActivity extends Activity {
     private TextToSpeech tts;
     File sdcard = Environment.getExternalStorageDirectory();
     String modelDir = "/sdcard/Captioner";
+    //String modelDir = "/Caffe_Mobile";
     String cnn_modelProto = modelDir + "/cnn_deploy.prototxt";
+    //String cnn_modelProto = modelDir + "/deploy.prototxt";
     String lstm_modelProto = modelDir + "/lstm_deploy.prototxt";
     String modelBinary = modelDir + "/cnn_lstm.caffemodel";
     String vocabulary = modelDir + "/vocabulary.txt";
@@ -65,7 +85,8 @@ public class MainActivity extends Activity {
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
     };
 
     /**
@@ -93,6 +114,15 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         verifyStoragePermissions(this);
         setContentView(R.layout.activity_main);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+
         tts = new TextToSpeech(this.getApplicationContext(), new TextToSpeech.OnInitListener()
         {
             @Override
@@ -104,14 +134,52 @@ public class MainActivity extends Activity {
                 }
             }
         });
+
+        mPicture = new Camera.PictureCallback() {
+
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+                if (pictureFile == null){
+                    Log.d(TAG, "Error creating media file, check storage permissions: ");// + e.getMessage());
+                    return;
+                }
+
+                try {
+                    FileOutputStream fos = new FileOutputStream(pictureFile);
+                    fos.write(data);
+                    fos.close();
+                    //added by gftm
+                    fileUri = Uri.fromFile(pictureFile);
+                    showDialogAndPredict(fileUri.getPath());
+                } catch (FileNotFoundException e) {
+                    Log.d(TAG, "File not found: " + e.getMessage());
+                } catch (IOException e) {
+                    Log.d(TAG, "Error accessing file: " + e.getMessage());
+                }
+            }
+        };
+        // Add a listener to the Capture button
+        captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        mCamera.takePicture(null, null, mPicture);
+                    }
+                }
+        );
+
+
         //tts.setLanguage(Locale.US);
         //tts.speak("a boy is playing football" +
-         //       "aa", TextToSpeech.QUEUE_FLUSH, null);
+        //       "aa", TextToSpeech.QUEUE_FLUSH, null);
 
-        ivCaptured = (ImageView) findViewById(R.id.ivCaptured);
+        //ivCaptured = (ImageView) findViewById(R.id.ivCaptured);
         tvLabel = (TextView) this.findViewById(R.id.tvlabel);
 
-        btnCamera = (Button) this.findViewById(R.id.id_btnCamera);
+        /*btnCamera = (Button) this.findViewById(R.id.id_btnCamera);
         btnCamera.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 initPrediction();
@@ -120,12 +188,27 @@ public class MainActivity extends Activity {
                 i.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
                 startActivityForResult(i, REQUEST_IMAGE_CAPTURE);
             }
+        });*/
+
+
+        btnRepeat = (Button) this.findViewById(R.id.id_btnRepeat);
+        btnRepeat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                preview.addView(mPreview);
+                btnSelect.setVisibility(View.VISIBLE);
+                captureButton.setVisibility(View.VISIBLE);
+                tvLabel.setText("");
+                tvLabel.setVisibility(View.GONE);
+                btnSpeak.setVisibility(View.GONE);
+                btnRepeat.setVisibility(View.GONE);
+            }
         });
 
         btnSelect = (Button) this.findViewById(R.id.id_btnSelect);
         btnSelect.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
-                initPrediction();
+                //initPrediction(); removed by gftm
                 Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(i, REQUEST_IMAGE_SELECT);
             }
@@ -137,7 +220,6 @@ public class MainActivity extends Activity {
                 speak_out();
             }
         });
-
 
         captioner = new Captioner();
         captioner.setNumThreads(32);
@@ -165,27 +247,38 @@ public class MainActivity extends Activity {
                 cursor.close();
             }
 
-            bmp = BitmapFactory.decodeFile(imgPath);
-            Log.d(LOG_TAG, imgPath);
-            Log.d(LOG_TAG, String.valueOf(bmp.getHeight()));
-            Log.d(LOG_TAG, String.valueOf(bmp.getWidth()));
+            showDialogAndPredict(imgPath);
 
-            dialog = ProgressDialog.show(MainActivity.this, "I am captioning...", "please wait..", true);
-
-            CNNTask cnnTask = new CNNTask( );
-            cnnTask.execute(imgPath);
-        } else {
-            btnCamera.setEnabled(true);
+        }/* else {
+            //btnCamera.setEnabled(true);
             btnSelect.setEnabled(true);
-        }
+        }*/
 
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+    private void showDialogAndPredict(String imgPath){
+        bmp = BitmapFactory.decodeFile(imgPath);
+        Log.d(LOG_TAG, imgPath);
+        Log.d(LOG_TAG, String.valueOf(bmp.getHeight()));
+        Log.d(LOG_TAG, String.valueOf(bmp.getWidth()));
+
+        preview.removeView(mPreview);
+        preview.setBackground(new BitmapDrawable(rotateBitmap(bmp,90.0f)));
+        captureButton.setVisibility(View.GONE);
+        btnSelect.setVisibility(View.GONE);
+
+        dialog = ProgressDialog.show(MainActivity.this, "I am captioning...", "Please wait.", true);
+
+        CNNTask cnnTask = new CNNTask( );
+        cnnTask.execute(imgPath);
+    }
+
     private void initPrediction() {
-        btnCamera.setEnabled(false);
-        btnSelect.setEnabled(false);
         tvLabel.setText("");
+        tvLabel.setVisibility(View.VISIBLE);
+        btnSpeak.setVisibility(View.VISIBLE);
+        btnRepeat.setVisibility(View.VISIBLE);
     }
 
     private void speak_out() {
@@ -219,19 +312,20 @@ public class MainActivity extends Activity {
      * display the results on screen
      */
     public void onTaskCompleted(String result) {
-        ivCaptured.setImageBitmap(bmp);
-        //ivCaptured.setPadding(3,3,3,3);
-        //ivCaptured.setBackgroundColor(Color.rgb(80, 255, 255));
-      //  tvLabel.setText(IMAGENET_CLASSES[result]);
+        initPrediction();
         tvLabel.setText(result);
-        //tvLabel.setPadding(5,5,5,5);
-        tvLabel.setBackgroundColor(Color.rgb(255, 255, 80));
+        tvLabel.setTextSize(18);
         tts.speak(result, TextToSpeech.QUEUE_ADD, null,null);
-        btnCamera.setEnabled(true);
-        btnSelect.setEnabled(true);
         if (dialog != null) {
             dialog.dismiss();
         }
+    }
+
+    public static Bitmap rotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
     }
 
     /**
@@ -286,5 +380,17 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 }
