91686c2daeaf8b1a88532d20340a130a82cde6e1
gftm <goncalomordido@gmail.com>
2017-07-23 12:16:22 +0100

    Updated UI

--- captioner/app/src/main/java/com/ImageCaptioning/CameraPreview.java
+++ captioner/app/src/main/java/com/ImageCaptioning/CameraPreview.java
@@ -55,7 +55,6 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
         // If your preview can change or rotate, take care of those events here.
         // Make sure to stop the preview before resizing or reformatting it.
-
         if (mHolder.getSurface() == null){
             // preview surface does not exist
             return;
